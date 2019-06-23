51455fd4729fa68fce24285c632463ae13103fa3
nakagawa333 <naka33321@gmail.con>
2019-06-23 14:39:23 +0900

    表の文字の色を白色に変更

--- TodoList/static/music.js
+++ TodoList/static/music.js
@@ -16,14 +16,15 @@ $(function(){
 		value = $("#"+i.toString()).text()
 		if(value=='A'){
 			$(".back:eq(" + i + ")").css("background-color","red");
+			$(".back:eq(" + i + ")").css("color","white");
 		}else if(value=='B'){
-           $(".back:eq(" + i + ")").css("background-color","yellow");
+           $(".back:eq(" + i + ")").css("background-color","orange");
         } else if(value == "C"){
-        	$(".back:eq(" + i + ")").css("background-color","pink");
+        	$(".back:eq(" + i + ")").css("background-color","yellow");
         } else if(value == "D"){
-        	$(".back:eq(" + i + ")").css("background-color","black");
-        } else if(value == "E"){
         	$(".back:eq(" + i + ")").css("background-color","blue");
+        } else if(value == "E"){
+        	$(".back:eq(" + i + ")").css("background-color","purple");
        }
 	}
 	//	alert("Hello");
